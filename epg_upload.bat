01260bdfb04f02deaeff0c932dfd3b03c9458425
serviciovodflex <serviciovodflex@gmail.com>
2023-08-31 05:36:18 -0600

    NXT Guide

--- epg_upload.bat
+++ epg_upload.bat
@@ -3,6 +3,8 @@ git add epg.xml
 git add epg-generic.xml
 git add img
 git add epg_upload.bat
+git add first-commit.bat
+git add git-pull.bat
 git commit -m "NXT Guide"
 git push -u origin master
 pause
---
+++ git-pull.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+git pull https://github.com/serviciovodflex/nxt-guide.git
+pause
+exit
